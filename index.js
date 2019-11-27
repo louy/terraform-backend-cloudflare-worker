475bb3c51e4ed01c5a24038ea18b6534b1049fd1
Louay Alakkad <louay_alakkad@mckinsey.com>
2019-11-27 17:12:43 +0000

    Add support for multiple states

--- index.js
+++ index.js
@@ -3,17 +3,16 @@
 const USERNAME = 'CHANGE ME!';
 const PASSWORD = 'CHANGE ME!';
 
-const STATE_ENDPOINT = '/';
 const UPDATE_METHOD = 'POST';
-const LOCK_ENDPOINT = '/';
+const LOCK_ENDPOINT_SUFFIX = '';
 const LOCK_METHOD = 'LOCK';
-const UNLOCK_ENDPOINT = '/';
+const UNLOCK_ENDPOINT_SUFFIX = '';
 const UNLOCK_METHOD = 'UNLOCK';
 
 // CF Worker KV configuration
 const STATE_NAMESPACE = TERRAFORM;
-const STATE_KEY = 'state';
-const LOCK_KEY = 'lock';
+const STATE_KEY_PREFIX = 'state::';
+const LOCK_KEY_PREFIX = 'lock::';
 
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
@@ -27,18 +26,19 @@ async function handleRequest(request) {
       if (authError) return authError;
     }
 
-    let requestURL = new URL(request.url)
+    let requestURL = new URL(request.url);
+    const path = requestURL.pathname;
     switch (true) {
-      case request.method === 'GET' && requestURL.pathname === STATE_ENDPOINT:
-        return await getState();
-      case request.method === UPDATE_METHOD && requestURL.pathname === STATE_ENDPOINT:
-        return await setState(await request.text());
-      case request.method === 'DELETE' && requestURL.pathname === STATE_ENDPOINT:
-        return await deleteState();
-      case request.method === LOCK_METHOD && requestURL.pathname === LOCK_ENDPOINT:
-        return await lockState(await request.text());
-      case request.method === UNLOCK_METHOD && requestURL.pathname === UNLOCK_ENDPOINT:
-        return await unlockState(await request.text());
+      case request.method === 'GET':
+        return await getState(path);
+      case request.method === UPDATE_METHOD:
+        return await setState(path, await request.text());
+      case request.method === 'DELETE':
+        return await deleteState(path);
+      case request.method === LOCK_METHOD && path.endsWith(LOCK_ENDPOINT_SUFFIX):
+        return await lockState(path.substr(0, path.length - LOCK_ENDPOINT_SUFFIX.length), await request.text());
+      case request.method === UNLOCK_METHOD && path.endsWith(UNLOCK_ENDPOINT_SUFFIX):
+        return await unlockState(path.substr(0, path.length - UNLOCK_ENDPOINT_SUFFIX.length), await request.text());
     }
     
     return new Response('Nothing found at ' + requestURL.pathname, {status: 404});
@@ -72,8 +72,8 @@ async function authenticate(request) {
   return void 0
 }
 
-async function getState() {
-  const state = await STATE_NAMESPACE.get(STATE_KEY);
+async function getState(path) {
+  const state = await STATE_NAMESPACE.get(STATE_KEY_PREFIX + path);
   if (!state) {
     return new Response('', {
       status: 404,
@@ -90,8 +90,8 @@ async function getState() {
     },
   });
 }
-async function setState(body) {
-  await STATE_NAMESPACE.put(STATE_KEY, body);
+async function setState(path, body) {
+  await STATE_NAMESPACE.put(STATE_KEY_PREFIX + path, body);
   return new Response(body || '', {
     status: 200,
     headers: {
@@ -100,8 +100,8 @@ async function setState(body) {
     },
   });
 }
-async function deleteState() {
-  await STATE_NAMESPACE.delete(STATE_KEY);
+async function deleteState(path) {
+  await STATE_NAMESPACE.delete(STATE_KEY_PREFIX + path);
   return new Response('', {
     status: 200,
     headers: {
@@ -110,8 +110,8 @@ async function deleteState() {
   });
 }
 
-async function lockState(body) {
-  const existingLock = await STATE_NAMESPACE.get(LOCK_KEY);
+async function lockState(path, body) {
+  const existingLock = await STATE_NAMESPACE.get(LOCK_KEY_PREFIX + path);
   if (existingLock) {
     return new Response(existingLock, {
       status: 423,
@@ -121,7 +121,7 @@ async function lockState(body) {
       },
     });
   }
-  await STATE_NAMESPACE.put(LOCK_KEY, body);
+  await STATE_NAMESPACE.put(LOCK_KEY_PREFIX + path, body);
   return new Response(body, {
     headers: {
       'Content-type': 'application/json',
@@ -130,8 +130,8 @@ async function lockState(body) {
   });
 }
 
-async function unlockState(body) {
-  await STATE_NAMESPACE.delete(LOCK_KEY);
+async function unlockState(path, body) {
+  await STATE_NAMESPACE.delete(LOCK_KEY_PREFIX + path);
   return new Response('', {
     headers: {
       'Cache-Control': 'no-store',
