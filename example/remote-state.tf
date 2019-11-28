06257ec70496f8aa48e29e7576f9476da71dc063
Louay Alakkad <louay_alakkad@mckinsey.com>
2019-11-28 13:35:24 +0000

    Ignore tfbackend files

--- example/remote-state.tf
+++ example/remote-state.tf
@@ -3,7 +3,7 @@ terraform {
     address = "https://terraform-backend.ACCOUNT_NAME.workers.dev/"
     username = "CHANGE ME!"
     password = "CHANGE ME!"
-    
+
     # Optional
     lock_address = "https://terraform-backend.ACCOUNT_NAME.workers.dev/"
     unlock_address = "https://terraform-backend.ACCOUNT_NAME.workers.dev/"
